bd52c8a6cbc822b784cf68a70b3de0799ac7757d
Katastrofi <117856699+Katastrofiii@users.noreply.github.com>
2023-08-11 23:30:24 -0700

    Add files via upload

---
+++ rps.js
@@ -0,0 +1,81 @@
+function myFunction() {
+    let randomNumber = Math.random();
+
+    let computerMove = '';
+
+    if (randomNumber > 0.33 && randomNumber <= 0.66) {
+        computerMove = "Scissor"
+    } else if (randomNumber > 0 && randomNumber <= 0.33) {
+        computerMove = "Paper"
+    } else if (randomNumber> 0.66 && randomNumber< 1) {
+        computerMove = "Rock"
+    }
+    document.getElementById('computer').innerHTML = "Computer move: " + computerMove;
+
+    let result = '';
+
+ if (computerMove === 'Rock') {
+   result = "tie";
+ } else if (computerMove === "Paper") {
+    result = "lose";
+ } else {
+    result = "win";
+ }
+
+ document.getElementById('value').innerHTML = "You "+result +" against the computer";
+}  
+
+
+function myFunction1() {
+    let randomNumber = Math.random();
+
+    let computerMove = '';
+
+    if (randomNumber > 0.33 && randomNumber <= 0.66) {
+        computerMove = "Scissor"
+    } else if (randomNumber > 0 && randomNumber <= 0.33) {
+        computerMove = "Paper"
+    } else if (randomNumber> 0.66 && randomNumber< 1) {
+        computerMove = "Rock"
+    }
+    document.getElementById('computer').innerHTML = "Computer move: " + computerMove;
+
+    let result = '';
+
+ if (computerMove === 'Paper') {
+   result = "tied";
+ } else if (computerMove === "Rock") {
+    result = "win";
+ } else {
+    result = "lose";
+ }
+
+ document.getElementById('value').innerHTML = "You "+result +" against the computer";
+}  
+
+function myFunction2() {
+    let randomNumber = Math.random();
+
+    let computerMove = '';
+
+    if (randomNumber > 0.33 && randomNumber <= 0.66) {
+        computerMove = "Scissor"
+    } else if (randomNumber > 0 && randomNumber <= 0.33) {
+        computerMove = "Paper"
+    } else if (randomNumber> 0.66 && randomNumber< 1) {
+        computerMove = "Rock"
+    }
+    document.getElementById('computer').innerHTML = "Computer move: " + computerMove;
+
+    let result = '';
+
+ if (computerMove === 'Scissor') {
+   result = "tied";
+ } else if (computerMove === "Paper") {
+    result = "win";
+ } else {
+    result = "lose";
+ }
+
+ document.getElementById('value').innerHTML = "You "+result +" against the computer";
+}
